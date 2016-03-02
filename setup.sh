df29442748cd69a813187c804e9b11f23157c794
ABaryshnikov <Alexander.Baryshnikov@billing.ru>
2016-03-02 15:32:36 +0300

    _

--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
-echo "Creating directories..."
-cd /home/box/web
-mkdir public
-mkdir uploads
-mkdir etc
-cd public
-mkdir img
-mkdir css
-mkdir js
-echo "Directories created!"
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+echo "Creating infrusructure..."
+sudo ﻿ln -s /home/box/web/etc/test.conf  /etc/nginx/sites-enabled/test.conf
+nginx -s reload
