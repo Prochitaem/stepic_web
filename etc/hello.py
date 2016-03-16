c52116bddc84c1d86e70d9f003d0dd0074c6ce13
ABaryshnikov <Alexander.Baryshnikov@billing.ru>
2016-03-16 17:53:40 +0300

    hello update

--- etc/hello.py
+++ etc/hello.py
@@ -1,9 +1,10 @@
 # -*- coding: UTF-8 -*-
-from urlparse import urlparse
 def app(environ, start_response):
       ##data = b"Hello, World!\n"
       query = environ['QUERY_STRING']
       data = query.split('&')
+      for i in range(len(data)):
+          data[i] += chr(13)
       start_response("200 OK", [
           ("Content-Type", "text/plain"),
           ("Content-Length", str(len(data)))
